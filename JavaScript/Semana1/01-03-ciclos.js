d43a14b97aad495abfe0b831f3bf8c1c24cb2f32
Noelia Romina Ruiz <estefitrinidad@gmail.com>
2023-04-08 03:23:42 -0300

    sacando For del mismo archivo

--- JavaScript/Semana1/01-03-ciclos.js
+++ JavaScript/Semana1/01-03-ciclos.js
@@ -1,10 +1,16 @@
-
-//ciclo for
-
-for (let contando = 0; contando < 3; contando++) {
-    console.log(contando);
+//While
+let contador = 0;
+while(contador < 3){
+    console.log(contador);
+    contador++;
 }
+console.log("Fin del ciclo -while-");
 
-console.log("fin del ciclo for")
-
+//do while
+let conteo = 0;
+do{          // el programa se ejecuta una vez
+    console.log(conteo); //se imprime el numero
+    conteo++;  //se incrementa
+}while(conteo < 3); //mientras la condicion sea menor a 3 se vuelve a ejecutar
+console.log("Fin del ciclo -do while-");
 
