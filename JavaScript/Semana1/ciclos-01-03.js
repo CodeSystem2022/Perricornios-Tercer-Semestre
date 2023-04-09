87c7652c389178ddbc3ad63c379c73b4b25d5370
Asterisko19 <103151514+asterisko19@users.noreply.github.com>
2023-04-08 22:00:29 -0300

    Etiquetas Labels

---
+++ JavaScript/Semana1/ciclos-01-03.js
@@ -0,0 +1,10 @@
+// La palabra continue y Etiquetas Labels
+inicio:
+for(let contando = 0; contando <= 10; contando++){
+    if(contando % 2 !== 0){
+        break inicio; //ir a la siguiente iteracion
+    }
+    console.log(contando);   0
+}
+console.log("Termina el ciclo");  //Termina el Ciclo
+
