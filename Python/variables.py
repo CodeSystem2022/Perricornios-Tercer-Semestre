de4d60defc8b8c67e6428bc0de813c8a0fa7bd52
Asterisko19 <103151514+asterisko19@users.noreply.github.com>
2023-04-20 21:51:11 -0300

    Clase 1.2 "Prueba"
    Creación de la clase: Película

---
+++ Python/variables.py
@@ -0,0 +1,18 @@
+#definiendo una variable con camelCase
+nombreCompletoDeTuTio = "Juany Encinas"
+
+#definiendo una variable con snake_case
+nombre_completo = "Juany"
+
+#concatenar con +
+bienvenida = "Hola "  +  "¿Como estas?"
+
+#concatenar con f-stings
+bienvenida = f"Hola {nombre_completo} ¿Como estas?"
+
+#operadores de pertenencia (in / not in)
+
+
+print(bienvenida)#imprimiendo 
+print("Juany" in bienvenida)#True
+print("Juany" not in bienvenida)#False
