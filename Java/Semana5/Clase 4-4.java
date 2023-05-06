af18ebc1b00ffdb5a17e9caa3998bd98779f0200
Nadia Soledad Pereyra <105832274+NaSP32@users.noreply.github.com>
2023-05-05 23:22:21 -0300

    1.3 Polimorfismo parte 2 (Video5)

--- Java/Semana5/Clase 4-4.java
+++ Java/Semana5/Clase 4-4.java
@@ -1,5 +1,3 @@
-import javax.crypto.interfaces.PBEKey;
-
 package test;
 import domain.*;
 
@@ -13,6 +11,12 @@ public class TestSobreescritura {
         //System.out.println("gerente1= "+ gerente1.obtenerDetalles());
     }
     public static void imprimir(Empleado empleado) {
-        System.out.println("Empleado= "+ empleado.obtenerDetalles);
+        System.out.println("Empleado= "+ empleado.obtenerDetalles());
     }
 }
+
+/* VIDEO 5 POLIMORFISMO: El principio del polimorfismo es tener m?todos m?s genericos
+donde solo con el metodo imprmir, se puede imprimir informacion de varios
+tipos de datos como aca de la clase padre y de la clase hija
+Cuando hacemos la sobreescritura de metodos, si el de la clase padre esta en protected, 
+en la clase hija el modificador de acceso no puede ser mas restrictivo:*/
