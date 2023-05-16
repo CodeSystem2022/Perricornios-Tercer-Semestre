ffd9e10d0257282066c2e434ab84ff7c74f72556
Carlos Villalón <111613524+VillaC95@users.noreply.github.com>
2023-05-16 19:28:06 -0300

    6.1 Sintaxis de Clases en JavaScript: Parte 2

---
+++ JavaScript/Semana7/TSLeccion05/05-03-clases.js
@@ -0,0 +1,11 @@
+class Persona{ //Definimos la clase.
+    constructor(nombre, apellido){ //Creamos el método constructor.
+        this.nombre = nombre; //Declaramos el atributo "nombre".
+        this.apellido = apellido; //Declaramos el atributo "apellido".
+    }
+}
+
+Let persona1 = new Persona("Martín, "Perez"); //Creamos el objeto "persona1".
+console.log(persona1); //Mostramos objeto persona1
+Let persona2 = new Persona("Carlos,"Lara"); //Creamos el objeto "persona2".
+console.log(persona2); //Mostramos objeto persona2
