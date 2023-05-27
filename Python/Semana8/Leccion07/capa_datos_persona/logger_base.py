054536c6331af2873533806013f6f2502b7d8fca
VillaC95 <carlos.villalon95616@gmail.com>
2023-05-27 13:14:54 -0300

    Realizo actividad 7.2 Manejo de logging: Parte 1

--- Python/Semana8/Leccion07/capa_datos_persona/logger_base.py
+++ Python/Semana8/Leccion07/capa_datos_persona/logger_base.py
@@ -2,6 +2,5 @@ import logging as log
 # log = loggin -> también puede hacerse así
 
 #esta va a ser la base para todos los demas archivos con el manejo de loggin
-#Configuración básica
-
+#Llamamos a una configuración básica
 log.basicConfig(level=log.DEBUG) #se van a ejecutar todos los mensajes a continuación
