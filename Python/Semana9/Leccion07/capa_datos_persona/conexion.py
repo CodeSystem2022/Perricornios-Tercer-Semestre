ae0b4ae5d2c366e569d7d49a4c8b6685ef25e797
Gabriel  Juan Alejandro Juhasz <88912420+GabJaJ@users.noreply.github.com>
2023-05-30 13:58:51 -0300

    8.3 Creación de la Clase Conexion: Parte 1, 2 y 3 - Video 5
    Actualizo:
    
    Python/Semana9/Leccion07/capa_datos_persona/
    conexion.py
    
    Responsabilidades:
    - Creacion del método  de clase para el Cursor. Generamos pruebas del código para la clase Conexion.

--- Python/Semana9/Leccion07/capa_datos_persona/conexion.py
+++ Python/Semana9/Leccion07/capa_datos_persona/conexion.py
@@ -12,24 +12,46 @@ class Conexion:
     _conexion = None
     _cursor = None
 
-#se crean los métodos
+    #se crean los métodos
 
-#Método para la conexion
-@classmethod
-def obtenerConexion(cls):
-    if cls._conexion is None:
-        try:
-            cls._conexion = bd.connect(host=cls._HOST,
-                                       user=cls._USERNAME,
-                                       password=cls._PASSWORD,
-                                       port=cls._DB_PORT,
-                                       database=cls.DATABASE)
-            log.debug(f'Conexión Exitosa: {cls._conexion}')
-            return cls._conexion
-        except Exception as e:
-            log.error(f'Ocurrió un error: {e}')
-            sys.exit()
-        else:
-            return cls._conexion
+    #Método para la conexion
+    @classmethod
+    def obtenerConexion(cls):
+        if cls._conexion is None:
+            try:
+                cls._conexion = bd.connect(host=cls._HOST,
+                                           user=cls._USERNAME,
+                                           password=cls._PASSWORD,
+                                           port=cls._DB_PORT,
+                                           database=cls._DATABASE)
+                log.debug(f'Conexión Exitosa: {cls._conexion}')
+                return cls._conexion   # mostramos el tipo de conexion
+            except Exception as e:
+                log.error(f'Ocurrió un error: {e}')
+                sys.exit()
+            else:
+                return cls._conexion
 
-#Método para el cursor
+    #Método para el cursor
+    # 8.3 Creación de la Clase Conexion: Video 5
+
+    @classmethod
+    def obtenerCursor(cls):
+        if cls._cursor is None:
+            try:
+                cls._cursor = cls.obtenerConexion().cursor()
+                log.debug(f'Se abrió correctamente el cursor: {cls._cursor}')
+                return cls._cursor
+            except Exception as e:
+                log.error(f'Ocurrió un error: {e}')
+                sys.exit()
+            else:
+                return cls._cursor
+
+
+# Prueba de errores de la clase Conection:
+
+if __name__ == '__main__':
+    Conexion.obtenerConexion()
+    Conexion.obtenerCursor()
+
