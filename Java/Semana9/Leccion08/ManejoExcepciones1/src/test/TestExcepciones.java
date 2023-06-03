de9762c19f64b086d162898ca0e58fea9a207b0b
Bruno L Cosimano <brunolcosimano95@gmail.com>
2023-06-02 21:56:16 -0300

    8.4 Creamos nuestra propia excepción video 9

--- Java/Semana9/Leccion08/ManejoExcepciones1/src/test/TestExcepciones.java
+++ Java/Semana9/Leccion08/ManejoExcepciones1/src/test/TestExcepciones.java
@@ -1,14 +1,17 @@
 
 package test;
 
+import static aritmetica.Aritmetica.division;
+
 public class TestExcepciones {
     public static void main(String[] args) {
         int resultado = 0;
         try{
-            resultado = 10/0;
+            resultado = division(10, 0);
         }catch(Exception e){
             System.out.println("Ocurrió un Error: ");
             e.printStackTrace(System.out); //permite imprimir en consola la pila de excepciones
+            System.out.println(e.getMessage());
         }
         System.out.println("La variable de resultado tiene como valor: "+resultado);
         //se ve el error ArithmeticException en tiempo de ejecución
