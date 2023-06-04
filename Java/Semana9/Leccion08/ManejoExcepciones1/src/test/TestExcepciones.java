e7808ad8365769639115cb7a00ca85842b505b54
Carlos Villalón <111613524+VillaC95@users.noreply.github.com>
2023-06-04 12:01:08 -0300

    Update TestExcepciones.java

--- Java/Semana9/Leccion08/ManejoExcepciones1/src/test/TestExcepciones.java
+++ Java/Semana9/Leccion08/ManejoExcepciones1/src/test/TestExcepciones.java
@@ -6,13 +6,13 @@ import static aritmetica.Aritmetica.division;
 public class TestExcepciones {
     public static void main(String[] args) {
         int resultado = 0;
-        try{
+        //try{
             resultado = division(10, 0);
-        }catch(Exception e){
-            System.out.println("Ocurrió un Error: ");
-            e.printStackTrace(System.out); //permite imprimir en consola la pila de excepciones
-            System.out.println(e.getMessage());
-        }
+//        }catch(Exception e){
+//            System.out.println("Ocurrió un Error: ");
+//            e.printStackTrace(System.out); //permite imprimir en consola la pila de excepciones
+//            System.out.println(e.getMessage());
+//        }
         System.out.println("La variable de resultado tiene como valor: "+resultado);
         //se ve el error ArithmeticException en tiempo de ejecución
         //como deriva de RuntimeException el error es de tipo uncheckException
