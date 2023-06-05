f62bd181b1f5ef1156134e2463d9a0696aaccfe5
Noe3467 <estefitrinidad@gmail.com>
2023-06-05 19:10:37 -0300

    9.1 En la clase PersonaDao: método seleccionar - (Video 1)

---
+++ Python/Semana10/Leccion07/capa_datos_persona/persona_dao.py
@@ -0,0 +1,37 @@
+from Leccion07.capa_datos_persona.Persona import Persona
+from Leccion07.capa_datos_persona.conexion import Conexion
+
+
+class PersonaDAO:
+    """
+    DAO signfica: Data Access Object
+    CRUD significa:
+                    Create -> Insertar
+                    Read -> Seleccionar
+                    Update -> Actualizar
+                    Delete -> Eliminar
+    """
+
+    _SELECCIONAR = 'SELECT + FROM persona ORDER BY id_persona'
+    _INSERTAR = 'INSERT INTO persona(nombre,apellido,email) VALUES (%s,%s,%s)'
+    _ACTUALIZAR = 'UPDATE persona SET nombre=%s, apellido=%s, email=%s WHERE id_persona=%s'
+    _ELIMINAR = 'DELETE FROM persona WHERE id_persona=%s'
+
+    #Clase 9: Lunes 5 de junio.
+
+    #Definimos los métodos de clase
+    @classmethod
+    def seleccionar(cls):
+        #armamos las conexiones automáticas
+        with Conexion.obtenerConexion():
+            #obtenemos el cursor también de manera automática
+            with Conexion.obtenerCursor() as cursor:
+                #creamos la query
+                cursor.execute(cls._SELECCIONAR) #ejecuta la sentencia
+                registros = cursor.fetchall()
+                personas = [] #creamos una lista de personas vacía
+                #recorremos el registro con un ciclo for each
+                for registro in registros:
+                     #cada registro es una columna (id_persona, nombre, apellido y email)
+                    persona = Persona(registro[0], registro[1], registro[2], registro[3])
+                    personas.append(persona)
