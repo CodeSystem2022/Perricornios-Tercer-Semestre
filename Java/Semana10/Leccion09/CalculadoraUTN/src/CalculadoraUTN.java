fdf1e1a3a2e086312b472345ab662697ee5a2c13
Bruno Cosimano <82295361+BrunoLCA95@users.noreply.github.com>
2023-06-07 20:24:08 -0300

    9.2 Definiendo las variables - Java - Video 2

--- Java/Semana10/Leccion09/CalculadoraUTN/src/CalculadoraUTN.java
+++ Java/Semana10/Leccion09/CalculadoraUTN/src/CalculadoraUTN.java
@@ -1,4 +1,11 @@
 
 public class CalculadoraUTN {
-    
+    public static void main(String[] args) {
+        System.out.println("******* aplicacion calculadora *******");
+        var operando1 = 10;
+        var operando2 = 20;
+        var resultado = operando1 + operando2;
+        System.out.println("resultado =" + resultado);
+        
+    }
 }
