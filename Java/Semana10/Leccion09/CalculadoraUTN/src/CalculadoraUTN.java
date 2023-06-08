bb4828076397208c4dcd16065d4328503fffc08c
Gabriel  Juan Alejandro Juhasz <88912420+GabJaJ@users.noreply.github.com>
2023-06-07 22:59:06 -0300

    9.4 Cargamos el menú - CalculadoraUTN - Video 4
    Actualizamos:
    
    Java/Semana10/Leccion09/CalculadoraUTN…/
    CalculadoraUTN.java
    
    Responsabilidades:
    -Creamos el Menú de la calculadora.

--- Java/Semana10/Leccion09/CalculadoraUTN/src/CalculadoraUTN.java
+++ Java/Semana10/Leccion09/CalculadoraUTN/src/CalculadoraUTN.java
@@ -6,15 +6,26 @@ public class CalculadoraUTN {
         Scanner entrada = new Scanner(System.in);
         
         System.out.println("******* aplicacion calculadora *******");
-        
-        System.out.print("Digite el valor para el operando 1: ");
-        var operando1 = Integer.parseInt(entrada.nextLine());
-        
-        System.out.print("Digite el valor para el operando 2: ");
-        var operando2 = Integer.parseInt(entrada.nextLine());
-        
-        var resultado = operando1 + operando2;
-        
-        System.out.println("resultado = " + resultado);  
+
+        //Mostramos el menú
+        System.out.println("""
+                1. Suma
+                2. Resta
+                3. Multiplicación
+                4. División
+                5. Salír.
+                """);
+        System.out.print("operación a realizar?: ");
+        var operacion = Integer.parseInt(entrada.nextLine());
+
+        if(operacion >= 1 && operacion <= 4) {
+
+            System.out.print("Digite el valor para el operando 1: ");
+            var operando1 = Integer.parseInt(entrada.nextLine());
+
+            System.out.print("Digite el valor para el operando 2: ");
+            var operando2 = Integer.parseInt(entrada.nextLine());
+
+        }
     }
 }
