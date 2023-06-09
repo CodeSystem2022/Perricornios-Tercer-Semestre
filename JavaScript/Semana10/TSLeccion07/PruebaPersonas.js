7f5c0b3295c8a7740c56f0ced8baca58c60c7f1b
VillaC95 <carlos.villalon95616@gmail.com>
2023-06-09 20:34:32 -0300

    9.6 Prueba Clase Persona - Video 6 - Js

--- JavaScript/Semana10/TSLeccion07/PruebaPersonas.js
+++ JavaScript/Semana10/TSLeccion07/PruebaPersonas.js
@@ -44,11 +44,7 @@ class Persona{
 
     //agregamos el toString()
     toString(){ 
-        return ` 
-        ${this._idPErsona} 
-        ${this._nombre} 
-        ${this._apellido} 
-        ${this._edad}`;
+        return `${this._idPErsona} ${this._nombre} ${this._apellido} ${this._edad}`;
     }
 
 }
@@ -112,13 +108,12 @@ class Cliente extends Persona{
     }
 
 }
+//Prueba Clase Persona
+let persona1 = new Persona('Juan','Perez', 32);
+console.log(persona1.toString());
 
-
-
-
-
-
-
+let persona2 = new Persona('Carla','Ortega', 22);
+console.log(persona2.toString());
 
 //Prueba clase Empleado
 let empleado1 = new Empleado('Pedro', 'Román', 18, 5000);
