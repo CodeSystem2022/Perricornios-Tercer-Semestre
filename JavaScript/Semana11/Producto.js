00556e46092aa543b042209e18516ebfafef6205
Gabriel  Juan Alejandro Juhasz <88912420+GabJaJ@users.noreply.github.com>
2023-06-13 16:31:03 -0300

    Creación Productos.js - ProyectoVentas - GJ
    Creación Productos.js - ProyectoVentas - para comparar el miércoles con los que se carguen en el campus.

---
+++ JavaScript/Semana11/Producto.js
@@ -0,0 +1,39 @@
+class Producto{
+
+    static contadorProductos = 0;
+
+    constructor(nombre, precio){
+        this,_idProducto = ++Producto.contadorProductos;
+        this._nombre = nombre;
+        this._precio = precio;
+    }
+
+    get idProducto(){
+        return this._idProducto;
+    }
+
+    get nombre(){
+        return this._nombre;
+    }
+
+    set nombre(nombre){
+        this._nombre = nombre;
+    }
+
+    get precio(){
+        return this._precio;
+    }
+
+    set precio(precio){
+        this._precio = precio;
+    }
+
+    toString(){
+        return `
+        ${super.toString()} 
+        ${this._idProducto} 
+        ${this._nombre};
+        ${this._precio}`;
+    }
+
+}
