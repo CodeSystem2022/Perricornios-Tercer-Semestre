ef5e466985520985a79ca83c192dfa26da71516e
Noe3467 <estefitrinidad@gmail.com>
2023-06-14 19:31:06 -0300

    10.2 Creación de la clase Persona -> (Video 4)

--- Java/Semana11/Leccion11/ListarPersonas/src/Persona.java
+++ Java/Semana11/Leccion11/ListarPersonas/src/Persona.java
@@ -1,4 +1,5 @@
 public class Persona {
+    private int id;
     private String nombre;
     private String tel;
     private String email;
@@ -8,4 +9,15 @@ public class Persona {
     public Persona(){
         this.id = ++Persona.numeroPersonas;//se incrementa desde el atributo de clase numeroPersonas
     }
+    
+    //Constructor con parámetros - Sobrecarga de constructores
+    public Persona(String nombre, String tel, String email){
+        //llamamos al constructor vacío
+        this(); //por default lo hace automáticamente
+        this.nombre = nombre;
+        this.tel = tel;
+        this.email = email;
+    }
+    
+    
 }
