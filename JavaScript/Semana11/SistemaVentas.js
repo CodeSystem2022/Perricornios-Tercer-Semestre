240a1721b301059b67e32401bd73d645df80c2ab
Noe3467 <estefitrinidad@gmail.com>
2023-06-15 23:18:42 -0300

    10.3 Creación de la Clase Producto (Video 2)

--- JavaScript/Semana11/SistemaVentas.js
+++ JavaScript/Semana11/SistemaVentas.js
@@ -35,8 +35,3 @@ class Producto{
 } //Fín de la clase Producto
 
 
-//Prueba de clase Producto
-let producto1 = new Producto('Pantalón', 200);
-let producto2 = new Producto('Camisa', 150);
-console.log(producto1.toString());
-console.log(producto2.toString());
