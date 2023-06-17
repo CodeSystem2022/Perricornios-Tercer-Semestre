f5d8df63e23e1818101f4d902763034fa020f17b
Braian Guzman Echarri <72713072+bguzmanech@users.noreply.github.com>
2023-06-17 20:03:51 -0300

    10.4 y Video 10 y 11

--- Java/Semana11/Leccion11/ListarPersonas/src/ListadoPersonasApp.java
+++ Java/Semana11/Leccion11/ListarPersonas/src/ListadoPersonasApp.java
@@ -11,9 +11,57 @@ public class ListadoPersonasApp {
         var salir = false;
         while(!salir){
             mostrarMenu(); //creamos un método
-
+            try{
+                salir = ejecutarOperacion(entrada, personas);
+            } catch (Exception e){
+                System.out.println("Ocurrio un error: " + e.getMessage());
+            }
+            System.out.println();
         }//Fin del ciclo while
     }// Fin método main
 
+    private static boolean ejecutarOperacion(Scanner entrada, List<Persona> personas) {
+        var opcion = Integer.parseInt(entrada.nextLine());
+        var salir = false;
+        //Revisamos la opcion digitada a traves de un switch
+        switch (opcion){
+            case 1 ->{
+                System.out.print("Digite el nombre: ");
+                var nombre = entrada.nextLine();
+                System.out.print("Digite el telefono: ");
+                var tel = entrada.nextLine();
+                System.out.print("Digite el correo: ");
+                var email = entrada.nextLine();
+                //creamos el objeto persona
+                var persona = new Persona(nombre, tel, email);
+                //Agregamos la persona a la lista
+                personas.add(persona);
+                System.out.println("La lista tiene: " + personas.size() + " elementos");
+            }//Fin caso 1
+            case 2 -> {//listar a las personas
+                System.out.println("Listado de personas: ");
+                //Mejoras con lambda y el metodo de referencia
+                //personas.forEach((persona) -> System.out.println(persona));
+                personas.forEach(System.out::println);
+            }
+            case 3 ->{
+                System.out.println("Hasta pronto..");
+                salir = true;
+            }
+            default -> System.out.println("Opcion incorrecta: " + opcion);
+        }
+        return salir;
+    }
+
+    private static void mostrarMenu(){
+        //mostramos las opciones
+        System.out.println("""
+               ****** Listado de Personas ******
+               1. Agregar
+               2. Listar
+               3. Salir
+                """);
+    }
 }
+
 
