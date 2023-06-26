1bf1cc8851b6d93ddb145adcc094baaf186681ee
Noe3467 <estefitrinidad@gmail.com>
2023-06-26 20:12:39 -0300

    11.3 Creamos la Clase CursorDelPool - Parte 2 (video 4)

--- Python/Semana13/Leccion07/capa_datos_persona/cursor_del_pool.py
+++ Python/Semana13/Leccion07/capa_datos_persona/cursor_del_pool.py
@@ -14,3 +14,17 @@ class CursorDelPool:
         self._conexion = Conexion.obtenerConexion()
         self._cursor = self._conexion.cursor()
         return self._cursor
+    
+    #Método dunder exit, renombramos los parámetros
+    def __exit__(self, tipo_exception, valor_exception, detalle_exception):
+        log.debug('Se ejecuta el método exit')
+
+        if valor_exception: #si es verdadero
+            self._conexion.rollback()
+            log.debug(f'Ocurrió una excepción: {valor_exception}')
+        else:
+            self._conexion.commit() #se hace el commit de la conexión
+            log.debug('Commit de la transacción')
+        self._cursor.close() #cerramos la conexión
+        Conexion.liberarConexion(self._conexion) # liberamos la conexión del pool de conexiones
+    
